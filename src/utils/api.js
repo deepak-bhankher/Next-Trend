dfbb43fbb60d17a7efa92e527583744e6e1fe35c
Deepak <deepakbhankhor758@gmail.com>
2026-08-08 10:44:49 +0530

    fix api base URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://next-trend-backend.onrender.com/",
+  baseURL: "https://next-trend-backend.onrender.com/api",
   withCredentials: true, // cookies (login token) bhejne ke liye zaroori
 });
 
